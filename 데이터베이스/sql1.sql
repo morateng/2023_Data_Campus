0b644ffe0cdebf1a7921a8b169d8ac0c74a413cd
KangJaeHoon <morateng@naver.com>
2023-07-06 15:45:03 +0900

    add sql1

---
+++ 데이터베이스/sql1.sql
@@ -0,0 +1,19 @@
+select publisher, price
+from book
+where bookname LIKE '축구의 역사';
+
+select phone
+from customer
+where name='김연아';
+
+select bookname, price
+from book;
+
+select * from customer;
+
+select *
+from book;
+
+select bookname
+from book
+where bookid=2;
